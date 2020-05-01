9ff3afab59223de1cbf2f82f82ebfc78d4d928fe
Shreeharsha Voonna <voonna.harshal@gmail.com>
2020-05-01 22:43:03 +0530

    Implementing Rest API's and auth using Flask-RESTful lib

---
+++ section4/security.py
@@ -0,0 +1,40 @@
+from user import User
+from werkzeug.security import safe_str_cmp
+
+users = [
+  # {
+  #   'id': 1,
+  #   'username': 'admin',
+  #   'password': 'password'
+  # }
+  User(1, 'admin', 'admin_password')
+]
+
+username_mapping = {
+  # 'admin' : {
+  #   'id': 1,
+  #   'username': 'admin',
+  #   'password': 'password'
+  # }
+  u.username : u for u in users
+}
+
+userid_mapping = {
+  # 1: {
+  #   'id': 1,
+  #   'username': 'admin',
+  #   'password': 'password'
+  # }
+  u.id : u for u in users
+}
+
+def authenticate(username, password):
+  user = username_mapping.get(username, None)
+  if user is not None and safe_str_cmp(user.password, password):
+    return user
+
+def identity(payload):
+  print(payload)
+  user_id = payload['identity']
+  return userid_mapping.get(user_id, None)
+
